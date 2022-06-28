a3d0ae4f77b71f41711672e5e8052153baf37dfe
Evan Jones <evanpeterjones@gmail.com>
2022-06-28 12:52:44 -0500

    updated Player to pass **kwargs instead of explicitly trying to pass the file_name which was redundant.

--- Models/Player.py
+++ Models/Player.py
@@ -4,14 +4,9 @@ from Models.ImageResource import ImageResource
 from Physics.Interactive import Interactive
 
 
-# had no clue why this wouldn't inherit the ImageResource class correctly.
-# turns out, with multiple inheritance, you have to provide the keyword arguments
-# explicitly, which I think is why it was not initializing properly
 class Player(Interactive, ImageResource):
-    def __init__(self, file_name=None, **kwargs):
-        super().__init__(file_name=file_name, **kwargs)
-        # may be more efficient to manually init both parent classes
-        # not sure
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def key(self, key, keydown=False):
         self.set_key_down(key, keydown)
